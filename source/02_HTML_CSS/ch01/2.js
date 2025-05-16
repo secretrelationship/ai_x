5a6cdc8a3a96869a34a349a46a45e465251b0e4c
secretrelationship <twinsyein@naver.com>
2025-05-16 18:00:11 +0900

    shopping mall

---
+++ source/02_HTML_CSS/ch01/2.js
@@ -0,0 +1,7 @@
+﻿//2.js
+/* 동적인 부분 */
+var name = prompt("이름은?", "김준서"); // 취소 클릭시 'null'리턴
+if (name != 'null' && name != '') {// 입력 후 확인 클릭시
+    document.write(name + '님 반갑습니다<br>');
+}
+// JavaScript source code
